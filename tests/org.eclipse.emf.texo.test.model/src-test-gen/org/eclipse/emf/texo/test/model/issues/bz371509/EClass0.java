7f1f7539140eef77a16887d10b943d1876dc764d
Martin Taal <mtaal@elver.org>
2012-03-09 16:15:07 +0100

    Solve failing bug

--- tests/org.eclipse.emf.texo.test.model/src-test-gen/org/eclipse/emf/texo/test/model/issues/bz371509/EClass0.java
+++ tests/org.eclipse.emf.texo.test.model/src-test-gen/org/eclipse/emf/texo/test/model/issues/bz371509/EClass0.java
@@ -11,6 +11,7 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OrderColumn;
+import org.eclipse.emf.texo.test.model.base.identifiable.Identifiable;
 
 /**
  * A representation of the model object '<em><b>EClass0</b></em>'. <!-- begin-user-doc --> <!-- end-user-doc -->
@@ -18,7 +19,7 @@ import javax.persistence.OrderColumn;
  * @generated
  */
 @Entity(name = "EClass0")
-public class EClass0 {
+public class EClass0 extends Identifiable {
 
   /**
    * <!-- begin-user-doc --> <!-- end-user-doc -->
--- tests/org.eclipse.emf.texo.test.model/src-test-gen/org/eclipse/emf/texo/test/model/issues/bz371509/EClass1.java
+++ tests/org.eclipse.emf.texo.test.model/src-test-gen/org/eclipse/emf/texo/test/model/issues/bz371509/EClass1.java
@@ -11,6 +11,7 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OrderColumn;
+import org.eclipse.emf.texo.test.model.base.identifiable.Identifiable;
 
 /**
  * A representation of the model object '<em><b>EClass1</b></em>'. <!-- begin-user-doc --> <!-- end-user-doc -->
@@ -18,7 +19,7 @@ import javax.persistence.OrderColumn;
  * @generated
  */
 @Entity(name = "EClass1")
-public class EClass1 {
+public class EClass1 extends Identifiable {
 
   /**
    * <!-- begin-user-doc --> <!-- end-user-doc -->
