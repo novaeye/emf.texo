8b617b5a917459ef64e6b9d47ebe1fb04f1ad072
Martin Taal <mtaal@elver.org>
2012-08-23 15:44:42 +0200

    Prevent exception when no content is posted in JSON operation, fixes issue 387553

--- runtime/org.eclipse.emf.texo.server/src/org/eclipse/emf/texo/server/service/json/JSONServiceContext.java
+++ runtime/org.eclipse.emf.texo.server/src/org/eclipse/emf/texo/server/service/json/JSONServiceContext.java
@@ -81,6 +81,9 @@ public class JSONServiceContext extends ServiceContext {
       converter.setObjectResolver(getObjectStore());
 
       content = content.trim();
+      if (content.length() == 0) {
+        return Collections.emptyList();
+      }
 
       final List<Object> resultList;
       if (content.startsWith("[")) { //$NON-NLS-1$
