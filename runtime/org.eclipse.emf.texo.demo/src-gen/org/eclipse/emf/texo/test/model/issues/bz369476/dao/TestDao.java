00b9b73ab5d9e76375f5aff471fd7f6427deaa6b
Martin Taal <mtaal@elver.org>
2013-01-11 21:18:06 +0100

    Added demo classes

---
+++ runtime/org.eclipse.emf.texo.demo/src-gen/org/eclipse/emf/texo/test/model/issues/bz369476/dao/TestDao.java
@@ -0,0 +1,30 @@
+package org.eclipse.emf.texo.test.model.issues.bz369476.dao;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.texo.server.store.BaseDao;
+import org.eclipse.emf.texo.test.model.issues.bz369476.Bz369476ModelPackage;
+import org.eclipse.emf.texo.test.model.issues.bz369476.Test;
+
+/**
+ * The Dao implementation for the model object '<em><b>Test</b></em>'.
+ * 
+ * @generated
+ */
+public class TestDao extends BaseDao<Test> {
+
+	/**
+	 * @generated
+	 */
+	@Override
+	public Class<Test> getEntityClass() {
+		return Test.class;
+	}
+
+	/**
+	 * @generated
+	 */
+	@Override
+	public EClass getEClass() {
+		return Bz369476ModelPackage.INSTANCE.getTestEClass();
+	}
+}
---
+++ runtime/org.eclipse.emf.texo.demo/src-gen/org/eclipse/emf/texo/test/model/samples/accounting/dao/VatDao.java
@@ -0,0 +1,30 @@
+package org.eclipse.emf.texo.test.model.samples.accounting.dao;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.texo.server.store.BaseDao;
+import org.eclipse.emf.texo.test.model.samples.accounting.AccountingModelPackage;
+import org.eclipse.emf.texo.test.model.samples.accounting.Vat;
+
+/**
+ * The Dao implementation for the model object '<em><b>Vat</b></em>'.
+ * 
+ * @generated
+ */
+public class VatDao extends BaseDao<Vat> {
+
+	/**
+	 * @generated
+	 */
+	@Override
+	public Class<Vat> getEntityClass() {
+		return Vat.class;
+	}
+
+	/**
+	 * @generated
+	 */
+	@Override
+	public EClass getEClass() {
+		return AccountingModelPackage.INSTANCE.getVatEClass();
+	}
+}
